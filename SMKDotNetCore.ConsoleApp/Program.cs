131bfe94ada664c0a3eabf1c348263c02e2ab9bc
SoemKyaw <soem22645@gmail.com>
2024-04-13 16:57:46 +0630

    [Complete] Ado.Net CRUD

--- SMKDotNetCore.ConsoleApp/Program.cs
+++ SMKDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SMKDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+// nuget
+// sql connection
+
+
+// Ado.Net
+// CRUD
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read(); 
+//adoDotNetExample.Create("Title 1", "Author 1", "content 1");
+//adoDotNetExample.Update(2004, "Title 4", "Author 4", "Content 4");
+//adoDotNetExample.Delete(2003);
+adoDotNetExample.Edit(2003);
+adoDotNetExample.Edit(2004);
+
+Console.ReadKey();
